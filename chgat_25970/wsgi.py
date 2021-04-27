757ffb3749c038db53fd80fc8006385c037719cf
Crowdbotics <team@crowdbotics.com>
2021-04-27 05:38:35 +0000

    Initial commit.

---
+++ chgat_25970/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for chgat_25970 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chgat_25970.settings')
+
+application = get_wsgi_application()
